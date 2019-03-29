384034ccd581021d4c07bb05eb35a525fdf76b50
khady94 <48485478+khady94@users.noreply.github.com>
2019-03-29 12:02:57 -0400

    Update numsConvert.py

--- numsConvert.py
+++ numsConvert.py
@@ -14,11 +14,26 @@ def num2string(num):
      numString = ""
 
      ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### this is the only section    ###
-     ### you change in this program. ###
-     ###################################
+     ### if num2string=1:
+print("1")
+elif num2string=2:
+print("2")
+elif num2string=3:
+print("3")
+elif num2string=4:
+print("4")
+elif num2string=5:
+print("5")
+elif num2string=6:
+print("6")
+elif num2string=7:
+print("7")
+elif num2string=8:
+print("8")
+elif num2string=9:
+print("9")
+     ###
+    
 
      return(numString)
 
